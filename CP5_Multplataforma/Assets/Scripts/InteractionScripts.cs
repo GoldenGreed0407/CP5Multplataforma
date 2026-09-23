8a284caea4b70696321b9fe02cd287e1e25a69e1
GoldenGreed0407 <joao.arruda0407@gmail.com>
2026-09-23 08:07:47 -0300

    UnityProject

---
+++ CP5_Multplataforma/Assets/Scripts/InteractionScripts.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InteractionScripts : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
